c5c9fb479d5ad01f015f53f4c2e9c775c8ad922f
David <davidcm097@gmail.com>
2021-01-31 21:29:49 +0000

    Initial commit

---
+++ src/main/java/dad/javafx/retro/Main.java
@@ -0,0 +1,8 @@
+package dad.javafx.retro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Pong.main(args);
+	}
+}
---
+++ src/main/java/view/ViewManager.java
@@ -0,0 +1,25 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Slider;
+public class ViewManager {
+
+	    @FXML
+	    private Slider slider_musc;
+
+	    @FXML
+	    private Slider slider_soni;
+
+	    @FXML
+	    private ComboBox<?> comboIdioma;
+
+	    @FXML
+	    private Button butAcept;
+
+	    @FXML
+	    private Button butVolv;
+
+	
+}
